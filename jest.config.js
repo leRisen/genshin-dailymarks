d1c9d21d55500642ce800e01a8f02942ba6800c7
leRisen <lemmas.online@gmail.com>
2022-05-06 00:05:32 +0300

    Changes for collect coverage

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,9 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
   preset: 'ts-jest',
+  collectCoverage: true,
   testEnvironment: 'node',
+  collectCoverageFrom: [
+    'src/**/*.ts',
+  ],
 };
